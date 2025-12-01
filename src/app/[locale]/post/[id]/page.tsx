3f85c6eb6c68281980a89b2e56cfd0ae7524bcdd
alpknx <alpknx@gmail.com>
2025-12-02 02:15:56 +0500

    chore: update post detail page

--- src/app/[locale]/post/[id]/page.tsx
+++ src/app/[locale]/post/[id]/page.tsx
@@ -34,7 +34,8 @@ export default async function PostDetailPage({ params, searchParams }: PostDetai
     notFound();
   }
 
-  const matchingMasters = await getMatchingMasters(id);
+  // Only load matching masters if not in profile mode (to improve performance)
+  const matchingMasters = source === 'profile' ? [] : await getMatchingMasters(id);
 
   return (
     <PostDetailClient post={post} matchingMasters={matchingMasters} source={source} />
