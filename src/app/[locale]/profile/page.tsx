2b8ada0c02e4a9347c5f36673e6d6b01781e711f
alpknx <alpknx@gmail.com>
2025-12-02 02:15:58 +0500

    chore: update profile page

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -10,7 +10,7 @@ export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user || session.user.role !== "master") {
-    redirect("/signin");
+    redirect("/signin?callbackUrl=/profile");
   }
 
   // Get master profile
